c7fa93de09cc3f67d8045e27fd804aa34a7f9535
0mar77 <luisomar7411@gmail.com>
2023-05-23 00:14:46 -0600

    Created React Portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Projects from './components/Project'
+import Footer from './components/Footer'
+import React, { useState } from 'react';
 
 function App() {
+
+  const [state, setState] = useState('AboutMe');
+
+  const changeState = (state) => { setState(state); }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <Header changeState={changeState} />
+
+      <div className='flex-container'>
+        <Projects state={state} />
+        <Footer />
+      </div>
+    
     </div>
   );
 }
